59bb6f6a689bd353f60a38a03bd7a73a3df54bd2
Ruben <76583185+mukherjeeruben@users.noreply.github.com>
2024-07-06 11:52:31 +0100

    Update app_config.py

--- plugins/hooks/config/app_config.py
+++ plugins/hooks/config/app_config.py
@@ -1,10 +1,10 @@
-SQL_SPECS ={"SQL_DATABASE" : "airflowpoc",
-            "SQL_HOST" : "aws.connect.psdb.cloud",
-            "SQL_PORT" : "3306",
-            "SQL_USER" : "cwbcsa2kssyxfgep4xeh",
-            "SQL_PASSWORD" : "pscale_pw_eDRDmeMCwboQPBGankrf6eqk6E9HEcgNwkY4I2LV1KK",
+SQL_SPECS ={"SQL_DATABASE" : "",
+            "SQL_HOST" : "",
+            "SQL_PORT" : "",
+            "SQL_USER" : "",
+            "SQL_PASSWORD" : "",
             "ca_path" : "/etc/ssl/certs/ca-certificates.crt"}
 
-DROP_BOX_SPECS = { "App_key" : "1c5nzbcenivjblu",
-                   "App_secret" : "igslvwc7udljtv0",
-                   "Access_Token": "sl.BomfMH6MICKJsu50R_X-1NxrK7wJIBOF83V4g3-BiX8vwg1XYrJqVZ-bhUdZR7AZBZ5WSb_LyBoJ8a0wH6xzNax4FmZ6ZFqSO52t0I_w_qs6_GIso7hyeeyQwtXl4Yirc2KWF0hNF1vt"}
+DROP_BOX_SPECS = { "App_key" : "",
+                   "App_secret" : "",
+                   "Access_Token": ""}
